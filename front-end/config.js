8864b1db236ee5b22d4bfd8e7ffec558922cbb65
Espen Lilleengen <espenlilleengen01@gmail.com>
2025-08-15 15:10:44 +0200

    Update API url

--- front-end/config.js
+++ front-end/config.js
@@ -1,5 +1,6 @@
 const config = {
-    API_BASE_URL: 'http://localhost:8000',
+    //API_BASE_URL: 'http://localhost:8000',
+    API_BASE_URL: 'https://image-recognition-q0g0.onrender.com',
 };
 
 export default config;
